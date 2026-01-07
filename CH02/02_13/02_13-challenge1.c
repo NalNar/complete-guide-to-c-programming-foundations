608a5f3cdfd33ce8ca4a6bf76b6deee64ed77b99
Nalin Narayan <nn2675@nyu.edu>
2026-01-07 11:10:48 +0000

    update

--- CH02/02_13/02_13-challenge1.c
+++ CH02/02_13/02_13-challenge1.c
@@ -4,18 +4,28 @@
 int main()
 {
 	//declare constant ratio equal to 3/4
+	const double ratio = 0.75;
 	//declare char a
+	char a;
 	//declare integer b
+	int b;
 	//declare float c
+	float c;
 
 	//assign char
+	a = 'A';
 	//assign integer
+	b = 90;
 	//assign float
+	c = 90.00;
 
 	//output char value, e.g., "the value of variable a is '?'"
-	//output integer value, e.g., "the value of variable b is ??"
-	//output float value, e.g., "the value of variable c is ???.??"
-	//output value of ratio, e.g., "the value of constant ratio is ???.??"
-
-	return 0;
+	printf("%c\n", a);
+			// output integer value, e.g., "the value of variable b is ??"
+			printf("%i\n", b);
+			// output float value, e.g., "the value of variable c is ???.??"
+			printf("%f\n", c);
+			// output value of ratio, e.g., "the value of constant ratio is ???.??"
+			printf("%f\n", ratio);
+			return 0;
 }
