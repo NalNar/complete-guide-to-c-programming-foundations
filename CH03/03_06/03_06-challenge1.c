e6464765e5c10696e10d77f1a33e618deb4febdd
Nalin Narayan <nn2675@nyu.edu>
2026-01-08 04:17:12 +0000

    update

--- CH03/03_06/03_06-challenge1.c
+++ CH03/03_06/03_06-challenge1.c
@@ -3,8 +3,11 @@
 int main()
 {
 	int artifacts,rooms,paintings,sculptures;
-
+rooms = 12;
+paintings = 16;
+sculptures = 4;
 	/* code your calculations here */
+	artifacts = rooms*paintings+sculptures;
 
 	printf("This museum has %d artifacts\n",artifacts);
 
